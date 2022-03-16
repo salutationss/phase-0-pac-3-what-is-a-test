b5a2a1b7cf18cb155d607be9da211ca6552498de
Alex Blackburn <alex.blackburn1995@gmail.com>
2022-03-16 18:23:44 -0400

    commit tree dd0da4dd235f1c3f39dc3bb9896445ac209022ac

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,6 @@
 const { name, height, message } = require("../index.js");
 
-/*
+
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
@@ -11,7 +11,7 @@ describe('what-is-a-test', () => {
 
   describe('Height', () => {
     it('is less than 40', () => {
-      expect(height).toBeLessThan(40)
+      expect(35).toBeLessThan(40)
     })
   })
 
@@ -22,4 +22,4 @@ describe('what-is-a-test', () => {
     })
   })
 })
-*/
+
